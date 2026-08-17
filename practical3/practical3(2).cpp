7c83df2254cd9ee3a9e30da6e9546fec0f416e0a
janviasodariya05 <janviasodariya05@gmail.com>
2026-08-17 15:29:05 +0530

    Add files via upload

---
+++ practical3/practical3(2).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void sortColors(int arr[], int n) {
+    int low = 0;
+    int mid = 0;
+    int high = n - 1;
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {2, 0, 2, 1, 1, 0};
+    int n = 6;
+
+    sortColors(arr, n);
+
+    cout << "Sorted colors: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
